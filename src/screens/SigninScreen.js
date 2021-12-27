c940f817117b6873abf3d9c2b9f742948cd5255c
ajaykumar.95@yahoo.com <ajaykumar.95@yahoo.com>
2021-12-27 13:32:48 +0530

    navigation hook-up

---
+++ src/screens/SigninScreen.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function SigninScreen() {
+    return (
+        <View>
+            <Text>SigninScreen</Text>
+        </View>
+    )
+}
