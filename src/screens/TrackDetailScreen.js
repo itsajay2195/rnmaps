70c3e94f7124b98ec324da35419e8d36fa080b87
ajaykumar.95@yahoo.com <ajaykumar.95@yahoo.com>
2022-01-01 17:00:11 +0530

    completion of the track detail screen

--- src/screens/TrackDetailScreen.js
+++ src/screens/TrackDetailScreen.js
@@ -1,15 +1,25 @@
 import React,{useContext} from 'react'
 import { View, Text } from 'react-native'
 import { Context as TrackContext } from '../context/TrackContext'
+import MapView, { Polyline } from 'react-native-maps'
 
 export default function TrackDetailScreen({navigation}) {
     const {state} = useContext(TrackContext)
     const _id = navigation.getParam('_id')
     const trackDetails = state.find(track => track._id === _id)
+    const initialCoords = trackDetails.locations[0].coords
    
     return (
-        <View>
-            <Text>{trackDetails.name}</Text>
-        </View>
+        <>
+            <Text style={{fontSize:48}}>{trackDetails.name}</Text>
+            <MapView style={{height:300}}
+             initialRegion={{
+                longitudeDelta: 0.01,
+                latitudeDelta: 0.01, 
+                ...initialCoords
+             }}   >
+                <Polyline coordinates={trackDetails.locations.map(loc => loc.coords)}/>
+            </MapView>
+        </>
     )
 }
